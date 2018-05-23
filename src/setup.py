e0cdf84f819bfa25385ab270da89674015e9d1c8
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-05-23 16:39:27 -0300

    agreguo la versión inicial del codigo de warden. muy basico y todavía no funciona

---
+++ src/setup.py
@@ -0,0 +1,39 @@
+"""
+    https://packaging.python.org/distributing/
+    https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    http://semver.org/
+
+    zero or more dev releases (denoted with a ”.devN” suffix)
+    zero or more alpha releases (denoted with a ”.aN” suffix)
+    zero or more beta releases (denoted with a ”.bN” suffix)
+    zero or more release candidates (denoted with a ”.rcN” suffix)
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='warden',
+          version='1.0.0.alpha0',
+          description='Aplicación para manejar los perfiles de los sistemas',
+          url='https://github.com/pablodanielrey/warden',
+          author='Desarrollo DiTeSi, FCE',
+          author_email='ditesi@econo.unlp.edu.ar',
+          classifiers=[
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5'
+          ],
+          packages=find_packages(exclude=['contrib', 'docs', 'test*']),
+          install_requires=['Flask',
+                            'flask_jsontools',
+                            'microservices_common'
+                            ],
+          entry_points={
+            'console_scripts': [
+                'flask=warden.web.main:main'
+            ]
+          }
+
+      )
---
+++ src/warden/web/main.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().propagate = True
+
+import json
+import flask
+from flask import Flask, request, send_from_directory, jsonify, redirect, session, url_for, make_response, render_template
+from flask_jsontools import jsonapi
+from werkzeug.contrib.fixers import ProxyFix
+
+import oidc
+from oidc.oidc import TokenIntrospection
+client_id = os.environ['OIDC_CLIENT_ID']
+client_secret = os.environ['OIDC_CLIENT_SECRET']
+rs = TokenIntrospection(client_id, client_secret)
+
+app = Flask(__name__, static_url_path='/src/warden/web')
+app.debug = False
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+API_BASE = os.environ['API_BASE']
+
+@app.route(API_BASE + '*', methods=['OPTIONS'])
+def options():
+    if request.method == 'OPTIONS':
+        return 204
+    return 204
+
+@app.route('/allowed', methods=['GET'])
+@rs.require_valid_token
+@jsonapi
+def allowed(token=None):
+    return {'allowed':True}
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+
+    '''
+        para autorizar el CORS
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+    '''
+    o = request.headers.get('Origin', None)
+    rm = request.headers.get('Access-Control-Request-Method', None)
+    rh = request.headers.get('Access-Control-Request-Headers', None)
+
+    r.headers['Access-Control-Allow-Methods'] = 'PUT,POST,GET,HEAD,DELETE'
+    r.headers['Access-Control-Allow-Origin'] = '*'
+    if rh:
+        r.headers['Access-Control-Allow-Headers'] = rh
+    r.headers['Access-Control-Max-Age'] = 1
+
+    return r
+
+def main():
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    main()
