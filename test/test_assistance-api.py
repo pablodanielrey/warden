850158f9cdcea1b32bbdfba615c6afe2e8bb6ea4
Miguel <miguel.macagno@gmail.com>
2019-05-30 13:26:28 -0300

    Se termina generacion de casos de testing de permisos denegados en base a los permisos efectivos de un usuario

--- test/test_assistance-api.py
+++ test/test_assistance-api.py
@@ -8,15 +8,19 @@ TEST ---> Se van a chequear permisos obtenidos al consultar UIDS ficticias:
         3 == AssistanceOperator
         4 == AssistanceUser
         5 == Default
-"""
 
+Para ello se establecen "permisos" que los usuarios tienen
+
+"""
+#Permisos existentes utilizados para crear casos de prueba
 prefijos = ['urn']
-sistemas = ['assistance','*']
-recursos = ['users','places','assistance-report','schedule','justifications','justification-date','justification-report','general-assistance-report','devices','logs','*']
-operaciones = ['create','read','update','delete','*']
-alcances = ['any','many', 'sub', 'one', 'self','*']
+sistemas = ['assistance']
+recursos = ['users','places','schedule','justifications','justification-date','assistance-report','justification-report','general-assistance-report','devices','logs']
+operaciones = ['create','read','update','delete']
+alcances = ['any','many', 'sub', 'one', 'self']
 modelos = ['restricted','']
 
+#Permisos efectivos de cada usuario
 permisos = {
         "sadm": [
             "urn:assistance:*"
@@ -50,47 +54,35 @@ permisos = {
             "urn:assistance:places:read"
         ],
         "default": [
-            "urn:*:*:*",
             "urn:assistance:users:read:self",
             "urn:assistance:users:read:many:restricted",
             "urn:assistance:places:read:many",
-            "urn:assistance:places:read:self",
             "urn:assistance:assistance-report:read:many:restricted",
-            "urn:assistance:justifications-report:read:many:restricted",
+            "urn:assistance:justification-report:read:many:restricted",
             "urn:assistance:general-assistance-report:read:many:restricted",
             "urn:assistance:schedule:read:self",
             "urn:assistance:schedule:read:many:restricted",
             "urn:assistance:justifications:read:many:restricted",
             "urn:assistance:justification-date:read:self",
             "urn:assistance:justification-date:create:many:restricted",
-            "urn:assistance:justification-date:delete:many:restricted",
-            #Permisos con error ----> VER
-            "urn:assistance:logs:update:any",
-            "urn:assistance:assistance-report:read:self:restricted",
-            "urn:assistance:justifications:read:self:restricted",
-            "urn:assistance:justification-date:create:self:restricted",
-            "urn:assistance:justification-date:read:many:restricted",
-            "urn:assistance:justification-date:update:many:restricted",
-            "urn:assistance:justification-date:update:self:restricted",
-            "urn:assistance:justification-date:delete:self:restricted",
-            "urn:assistance:justification-date:*:many:restricted",
-            "urn:assistance:general-assistance-report:read:self:restricted",
-            
-            
+            "urn:assistance:justification-date:delete:many:restricted"           
         ]
     }
 
 
 def test_default():
-    import warden.api.rest.permisos as p
-
     ''' Comprobacion permisos de usuario default
+    Se crean 3 tests diferentes para usuario default.
+    1 --> Permisos que tiene y que deben retornar --> VERDADERO
+    2 --> Permisos sobre recursos que NO tiene debe retornar --> FALSO
+    3 --> Permisos DENEGADOS generados automaticamente en base al permiso de la persona que deben retornar --> FALSO
+
     Permisos de un usuario default:
     "urn:assistance:users:read:self",
     "urn:assistance:users:read:many:restricted",
     "urn:assistance:places:read:many",
     "urn:assistance:assistance-report:read:many:restricted",
-    "urn:assistance:justifications-report:read:many:restricted",
+    "urn:assistance:justification-report:read:many:restricted",
     "urn:assistance:general-assistance-report:read:many:restricted",
     "urn:assistance:schedule:read:self",
     "urn:assistance:schedule:read:many:restricted",
@@ -98,9 +90,8 @@ def test_default():
     "urn:assistance:justification-date:create:many:restricted",
     "urn:assistance:justification-date:delete:many:restricted",
     "urn:assistance:justification-date:read:self"  
-
-    OJO CON EL ORDEN DE ASIGNACION DE PERMISOS PARA UN USUARIO
     '''
+    import warden.api.rest.permisos as p
     
     ''' Accesos permitidos ''' 
     assert p.chequear_permisos('default', [
@@ -108,7 +99,7 @@ def test_default():
                                             "urn:assistance:users:read:self",
                                             "urn:assistance:places:read:many",
                                             "urn:assistance:assistance-report:read:many:restricted",
-                                            "urn:assistance:justifications-report:read:many:restricted",
+                                            "urn:assistance:justification-report:read:many:restricted",
                                             "urn:assistance:general-assistance-report:read:many:restricted",
                                             "urn:assistance:schedule:read:many:restricted",
                                             "urn:assistance:schedule:read:self",
@@ -121,7 +112,7 @@ def test_default():
                                                 "urn:assistance:users:read:self",
                                                 "urn:assistance:places:read:many",
                                                 "urn:assistance:assistance-report:read:many:restricted",
-                                                "urn:assistance:justifications-report:read:many:restricted",
+                                                "urn:assistance:justification-report:read:many:restricted",
                                                 "urn:assistance:general-assistance-report:read:many:restricted",
                                                 "urn:assistance:schedule:read:many:restricted",
                                                 "urn:assistance:schedule:read:self",
@@ -143,6 +134,7 @@ def test_default():
                 for a in alcancesDenegados:
                     assert p.chequear_permisos('default', [f"urn:{s}:{r}:{o}:{a}"], permisos) == (False, set())
 
+    ''' Permisos denegados generados automaticamente en base al perfil enviado '''
     #Combinaciones que deberian ser denegadas para el perfil determinado
     denegados = generarPermisosDenegados('default')
     for urn in denegados:
@@ -152,16 +144,34 @@ def test_default():
                     for a in denegados[urn][s][r][o]:
                         for m in denegados[urn][s][r][o][a]:
                             if m == 'restricted':
-                                print(f'{urn}:{s}:{r}:{o}:{a}:{m}')
+                                print(f'Probando permiso --> {urn}:{s}:{r}:{o}:{a}:{m}')
                                 assert p.chequear_permisos('default',[f'{urn}:{s}:{r}:{o}:{a}:{m}'], permisos) == (False, set())
                             else:
-                                print(f'{urn}:{s}:{r}:{o}:{a}')
+                                print(f'Probando permiso --> {urn}:{s}:{r}:{o}:{a}')
                                 assert p.chequear_permisos('default',[f'{urn}:{s}:{r}:{o}:{a}'], permisos) == (False, set())
 
 def generarPermisosDenegados(usuario):
     """
     Genera un arbol de los permisos denegados para el usuario solicitado
     """
+    
+    equivalencias_alcance = {
+        'self': ['self'],
+        'one': ['one','self'],
+        'sub': ['sub','self'],
+        'many': ['many','sub','one','self'],
+        'any': ['any','many', 'sub', 'one', 'self'],
+        '*': ['any','many', 'sub', 'one', 'self']
+    }
+
+    equivalencias_operaciones = {
+        'delete': ['delete'],
+        'update': ['update'],
+        'read': ['read'],
+        'create': ['create','read','update'],
+        '*': ['create','read','update','delete']
+    }
+
     contadorTotales = 0
     arbolPermisos = {}
     #Creo arbol de permisos completo
@@ -187,87 +197,62 @@ def generarPermisosDenegados(usuario):
         sistema  = parseado[1] if len(parseado) >= 2 else None
         recurso  = parseado[2] if len(parseado) >= 3 else None
         operacion= parseado[3] if len(parseado) >= 4 else None
-        alcance  = parseado[4] if len(parseado) >= 5 else None
+        alcance  = parseado[4] if len(parseado) >= 5 else '*'
         modelo   = parseado[5] if len(parseado) == 6 else None
+        
+        print(f'Permiso del usuario ----->{urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
 
         #Voy consultando por alcence del permiso, si existe lo elimino de la lista de no permitidos
-        if urn in arbolPermisos.keys():    
-            if sistema in arbolPermisos[urn].keys():
-                if (not recurso or recurso == '*') and (not operacion or operacion == '*') and (not alcance or alcance == '*' or alcance == 'any' ) and (not modelo):
-                    print(f'Eliminando SISTEMA ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                    arbolPermisos[urn].pop(sistema)
-                elif recurso in arbolPermisos[urn][sistema].keys():
-                    if (not operacion or operacion == '*') and (not alcance or alcance == '*' or alcance == 'any') and (not modelo):
-                        print(f'Eliminando RECURSO ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                        arbolPermisos[urn][sistema].pop(recurso)
-                    elif operacion in arbolPermisos[urn][sistema][recurso].keys():
-                        if (not alcance or alcance == '*' or alcance == 'any'):
-                            if not modelo:
-                                print(f'Eliminando OPERACION ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                arbolPermisos[urn][sistema][recurso].pop(operacion)
-                            #Si es any o * y es restricted entonces elimino los restricted de todos los alcances
-                            elif not alcance or alcance == 'any' or alcance == '*':
-                                if modelo in arbolPermisos[urn][sistema][recurso][operacion]['many']:
-                                    arbolPermisos[urn][sistema][recurso][operacion]['many'].remove(modelo)
-                                if modelo in arbolPermisos[urn][sistema][recurso][operacion]['sub']:
-                                    arbolPermisos[urn][sistema][recurso][operacion]['sub'].remove(modelo)
-                                if modelo in arbolPermisos[urn][sistema][recurso][operacion]['one']:
-                                    arbolPermisos[urn][sistema][recurso][operacion]['one'].remove(modelo)
-                                if modelo in arbolPermisos[urn][sistema][recurso][operacion]['self']:
-                                    arbolPermisos[urn][sistema][recurso][operacion]['self'].remove(modelo)
-                                if modelo in arbolPermisos[urn][sistema][recurso][operacion]['self']:
-                                    arbolPermisos[urn][sistema][recurso][operacion]['self'].remove(modelo)
-                        elif alcance == 'many':
-                            if 'sub' in arbolPermisos[urn][sistema][recurso][operacion].keys():
-                                #Si el modelo es restricted solamente elimino los restricted hijos de sub, sino elimino el alcance completo
-                                if modelo == 'restricted' and modelo in arbolPermisos[urn][sistema][recurso][operacion]['sub']:
-                                    print(f'Eliminando SUB -> RESTRICTED POR MANY -> RESTRICTED ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion]['sub'].remove(modelo)        
-                                else:
-                                    print(f'Eliminando ALCANCE SUB POR MANY----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion].pop('sub')
-                            if 'one' in arbolPermisos[urn][sistema][recurso][operacion].keys():
-                                #Si el modelo es restricted solamente elimino los restricted hijos de one, sino elimino el alcance one completo
-                                if modelo == 'restricted' and modelo in arbolPermisos[urn][sistema][recurso][operacion]['one']:
-                                    print(f'Eliminando ONE -> RESTRICTED POR MANY -> RESTRICTED ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion]['one'].remove(modelo)
-                                else:
-                                    print(f'Eliminando ALCANCE ONE POR MANY----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion].pop('one')
-                            #Si existe el alcance para la operacion...
-                            if alcance in arbolPermisos[urn][sistema][recurso][operacion].keys():
-                                #Si no existe el modelo es para TODO entonces elimino el calcance
-                                if (not modelo):
-                                    print(f'Eliminando ALCANCE ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion].pop(alcance)
-                                #Sino solo elimino el modelo
-                                elif modelo in arbolPermisos[urn][sistema][recurso][operacion][alcance]:
-                                    print(f'Eliminando MODELO ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion][alcance].remove(modelo)
-                        elif alcance in ['one','sub','self']:
-                            #Si existe el alcance para la operacion...
-                            if alcance in arbolPermisos[urn][sistema][recurso][operacion].keys():
-                                #Si no existe el modelo es para TODO entonces elimino el calcance
-                                if (not modelo):
-                                    print(f'Eliminando ALCANCE ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion].pop(alcance)
-                                #Sino solo elimino el modelo
-                                elif modelo in arbolPermisos[urn][sistema][recurso][operacion][alcance]:
-                                    print(f'Eliminando MODELO ----> {urn}:{sistema}:{recurso}:{operacion}:{alcance}:{modelo}')
-                                    arbolPermisos[urn][sistema][recurso][operacion][alcance].remove(modelo)
-
+        if urn in arbolPermisos.keys():
+            #Si es * son todos los sistemas entonces creo un arreglo de los sistemas existentes para recorrerlo 
+            if sistema == '*':
+                sistemasTmp = arbolPermisos[urn].keys()
+            #Sino creo un arreglo del sistema actual
+            else:
+                sistemasTmp = [sistema]
+            #Recorro el o los sistemas dependiendo el caso
+            for si in sistemasTmp:
+                #Si existe en el arbol de permisos entro
+                if si in arbolPermisos[urn].keys():
+                    #Si es * son todos los recursos entonces creo un arreglo de los recursos existentes para recorrerlo
+                    if recurso == '*':
+                        recursosTmp = arbolPermisos[urn][si].keys()
+                    #Sino creo un arreglo del recurso actual
+                    else:
+                        recursosTmp = [recurso]
+                    #Recorro todos los recursos dependiendo el caso
+                    for re in recursosTmp:
+                        #Si existe en el arbol entro
+                        if re in arbolPermisos[urn][si].keys():
+                            #Por cada equivalencia de operacion entro
+                            for op in equivalencias_operaciones[operacion]:
+                                #Si existe la operacion para el recurso entro
+                                if op in arbolPermisos[urn][si][re].keys():
+                                    #Por cada equivalencia de alcance entro
+                                    for al in equivalencias_alcance[alcance]:
+                                        #Si existe el alcance para para la operacion
+                                        if al in arbolPermisos[urn][si][re][op].keys():    
+                                            #Si no esta definido el modelo entonces son todos los alcances elimino todo si existe
+                                            if not modelo:
+                                                print(f'Eliminando ALCANCE ----> {urn}:{si}:{re}:{op}:{al}:{modelo}')
+                                                arbolPermisos[urn][si][re][op].pop(al)
+                                            #Si es any o * y es restricted entonces elimino los restricted de todos los alcances
+                                            else:
+                                                if modelo in arbolPermisos[urn][si][re][op][al]:
+                                                    print(f'Eliminando MODELO ----> {urn}:{si}:{re}:{op}:{al}:{modelo}')
+                                                    arbolPermisos[urn][si][re][op][al].remove(modelo)
     return arbolPermisos
 
-if __name__ == "__main__":
-    usuario = 'default'
-    denegados = generarPermisosDenegados(usuario)
-    contador = 0
-    for urn in denegados:
-        for s in denegados[urn]:
-            for r in denegados[urn][s]:
-                for o in denegados[urn][s][r]:
-                    for a in denegados[urn][s][r][o]:
-                        for m in denegados[urn][s][r][o][a]:
-                            print(f'{urn}:{s}:{r}:{o}:{a}:{m}')
-                            contador += 1
-    print(f'Permisos totales denegados para el usuario: {contador}')
+#if __name__ == "__main__":
+#    usuario = 'default'
+#    denegados = generarPermisosDenegados(usuario)
+#    contador = 0
+#    for urn in denegados:
+#        for s in denegados[urn]:
+#            for r in denegados[urn][s]:
+#                for o in denegados[urn][s][r]:
+#                    for a in denegados[urn][s][r][o]:
+#                        for m in denegados[urn][s][r][o][a]:
+#                            #print(f'{urn}:{s}:{r}:{o}:{a}:{m}')
+#                            contador += 1
+#    print(f'Permisos totales denegados para el usuario: {contador}')        
